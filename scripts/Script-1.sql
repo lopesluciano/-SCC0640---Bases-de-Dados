978753177413c09ca5d71d01c552aadf823fd05f
lopesluciano <lpsfluciano@gmail.com>
2024-10-27 16:24:28 -0300

    Initial commit

---
+++ scripts/Script-1.sql
@@ -0,0 +1,6 @@
+create table aluno (
+    cpf CHAR(14),
+    CONSTRAINT pk_aluno PRIMARY KEY (cpf)
+);
+
+drop table aluno;
---
+++ scripts/Script.sql
@@ -0,0 +1,4 @@
+CREATE TABLE aluno (
+    cpf CHAR(14),
+    CONSTRAINT pk_aluno PRIMARY KEY (cpf)
+);
