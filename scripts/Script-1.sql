717d36fe0e3ffcb0741931618f58ecfef03e1bd5
lopesluciano <lpsfluciano@gmail.com>
2024-10-29 13:57:03 -0300

    Mudanca no Compose.yml

--- scripts/Script-1.sql
+++ scripts/Script-1.sql
@@ -3,4 +3,9 @@ create table aluno (
     CONSTRAINT pk_aluno PRIMARY KEY (cpf)
 );
 
+create table professor (
+    cpf CHAR(14),
+    CONSTRAINT pk_professor PRIMARY KEY (cpf)
+);
+
 drop table aluno;
